829a15d28876c16a32f4738d87e811ca0b9317c7
JhonatanHzB <jhonatanhzb@outlook.com>
2023-12-04 18:07:19 -0600

    feat: integration the cart into the store

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
+import { Cart } from "../model/cart.model";
 
 @Component({
     selector: "store",
@@ -11,7 +12,7 @@ export class StoreComponent {
     productsPerPage = 4;
     selectedPage = 1;
 
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository, private cart: Cart) { }
 
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -36,15 +37,13 @@ export class StoreComponent {
         this.changePage(1);
     }
 
-    // get pageNumbers(): number[] {
-    //     return Array(Math.ceil(this.repository
-    //         .getProducts(this.selectedCategory).length / this.productsPerPage))
-    //             .fill(0).map((x, i) => i + 1);
-    // }
-
     get pageCount(): number {
         return Math.ceil(this.repository
             .getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
 
 }
