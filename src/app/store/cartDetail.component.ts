15f7c656d81a198178244808ee90cf373ef5bb9a
JhonatanHzB <jhonatanhzb@outlook.com>
2023-12-05 19:57:43 -0600

    update: completing cart detail feature

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
 
 @Component({
-    template: `<div><h3 class="bg-info p-1 text-white">Cart Detail Component</h3></div>`
+    templateUrl: "cartDetail.component.html"
 })
-export class CartDetailComponent { }
+export class CartDetailComponent {
+
+    constructor(public cart: Cart) { }
+
+}
