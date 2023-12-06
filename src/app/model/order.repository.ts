480628dfead991bf261a3d97b6cafec3de4083b9
JhonatanHzB <jhonatanhzb@outlook.com>
2023-12-06 11:21:20 -0600

    feat: extending the model with orders

---
+++ src/app/model/order.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { Order } from "./order.model";
+import { Observable } from "rxjs";
+import { StaticDataSource } from "./static.datasource";
+
+@Injectable()
+export class OrderRepository {
+
+    private order: Order[] = [];
+
+    constructor(private dataSource: StaticDataSource) { }
+
+    getOrders(): Order[] {
+        return this.order;
+    }
+
+    saveOrder(order: Order): Observable<Order> {
+        return this.dataSource.saveOrder(order);
+    }
+
+}
